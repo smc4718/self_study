e31089e2b1c2de2d0b1f2993bf903121fde5d4f0
smc4718 <smc4718@naver.com>
2024-03-10 21:44:59 +0900

    study11-3

--- core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 import java.util.HashMap;
 import java.util.Map;
 
-@Component
+@Component // 스프링 빈 등록되면, memoryMemberRepository 로 이름이 등록된다.
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
--- core/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
+++ core/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -1,31 +1,30 @@
 package hello.core.scan.filter;
-
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import
+        org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.springframework.context.annotation.ComponentScan.*;
-
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.context.annotation.ComponentScan.Filter;
 public class ComponentFilterAppConfigTest {
-
     @Test
     void filterScan() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        ApplicationContext ac = new
+                AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
         BeanA beanA = ac.getBean("beanA", BeanA.class);
         assertThat(beanA).isNotNull();
-
-        ac.getBean("beanB", BeanB.class);
+        Assertions.assertThrows(
+                NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("beanB", BeanB.class));
     }
-
     @Configuration
     @ComponentScan(
-            includeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class),
-            excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class)
+            includeFilters = @Filter(classes = MyIncludeComponent.class),
+            excludeFilters = @Filter(classes = MyExcludeComponent.class)
     )
     static class ComponentFilterAppConfig {
     }
