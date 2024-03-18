56d1da2701e5e63395b27d8b133b8ab5c5578888
smc4718 <smc4718@naver.com>
2024-03-19 01:34:46 +0900

    옵션 처리

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,38 +8,54 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class OrderServiceImpl implements OrderService{
+public class OrderServiceImpl implements OrderService {
 
     private MemberRepository memberRepository;
     private DiscountPolicy discountPolicy;
 
-//  3.필드 주입 방법 (쓰면 안됨, 사용하지 말자!, 굳이 쓴다면 애플리케이션의 실제 코드와 관계 없는 테스트 코드에서만 사용해라)
-//   @Autowired private MemberRepository memberRepository;
-//   @Autowired private DiscountPolicy discountPolicy;
+/*
+  3.필드 주입 방법 (쓰면 안됨, 사용하지 말자!, 굳이 쓴다면 애플리케이션의 실제 코드와 관계 없는 테스트 코드에서만 사용해라)
+   @Autowired private MemberRepository memberRepository;
+   @Autowired private DiscountPolicy discountPolicy;
+*/
 
-//  2.수정자 주입 (setter 주입) 방법
-//    @Autowired(required = false)
-//    public void setMemberRepository(MemberRepository memberRepository) {
-//        this.memberRepository = memberRepository;
-//    }
-//
-//    @Autowired
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-//        this.discountPolicy = discountPolicy;
-//    }
+/*
+  2.수정자 주입 (setter 주입) 방법
+    @Autowired(required = false)
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
-//  1.생성자 주입 방법
-//    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
 
+*/
+
+/*
+// 1.생성자 주입 방법
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+*/
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+
+// 4.일반 메서드 주입
     @Autowired
     public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
