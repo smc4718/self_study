81d5db1bdc20f01b4c25e1d1b45146880ddc5242
smc4718 <smc4718@naver.com>
2024-04-01 03:50:54 +0900

    Hello 서블릿

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan	//서블릿 자동 등록 (스프링이 자동으로 패키지 포함해서 하위 패키지 다 뒤져서 서블릿을 다 찾아서 자동으로 서블릿을 등록해서 실행할 수 있도록 도와줍니다.)
 @SpringBootApplication
 public class ServletApplication {
 
