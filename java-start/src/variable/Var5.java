99d0ec113bdb7af6800df11b129fab5aa768a8fd
smc4718 <smc4718@naver.com>
2024-04-26 17:10:23 +0900

    변수

---
+++ java-start/src/variable/Var5.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class Var5 {
+
+    public static void main(String[] args) {
+        int a;  // 선언
+        a = 1;  // 초기화
+        System.out.println(a);  // 출력
+
+        int b = 2; // 변수 선언과 초기화를 한 번에.
+        System.out.println(b);
+
+        int c = 3, d= 4; // 여러 변수 선언과 초기화를 한 번에.
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
---
+++ java-start/src/variable/ex/VarEx3.java
@@ -0,0 +1,12 @@
+package variable.ex;
+
+public class VarEx3 {
+
+    public static void main(String[] args) {
+        long var = 10000000000L;
+        System.out.println(var);
+
+        boolean var2 = true;;
+        System.out.println(var2);
+    }
+}
