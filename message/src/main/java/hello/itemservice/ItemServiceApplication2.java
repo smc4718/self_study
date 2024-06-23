6b951a5c441e08d301c1468e6702201fd9033124
smc4718 <smc4718@naver.com>
2024-06-24 01:08:15 +0900

    메세지 국제화 소스

---
+++ message/src/main/java/hello/itemservice/ItemServiceApplication2.java
@@ -0,0 +1,13 @@
+package hello.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceApplication2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceApplication2.class, args);
+	}
+
+}
