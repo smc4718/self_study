ca1d29e26597be7d1df371b60cf47f87edaab05b
smc4718 <smc4718@naver.com>
2024-08-06 01:59:23 +0900

    검증4

---
+++ message/src/main/java/hello/itemservice/ItemServiceApplication4.java
@@ -0,0 +1,13 @@
+package hello.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceApplication4 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceApplication4.class, args);
+	}
+
+}
