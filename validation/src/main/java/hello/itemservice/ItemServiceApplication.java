f1133a46d3f3be685cb58e9a6967244efa3a337c
smc4718 <smc4718@naver.com>
2024-08-12 00:01:25 +0900

    Validation

--- validation/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ validation/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ItemServiceApplication5 {
+public class ItemServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ItemServiceApplication5.class, args);
+		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
 }
