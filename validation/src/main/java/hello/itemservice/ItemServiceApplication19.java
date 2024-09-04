d586b224ce26fa46d7620784bdec810f0d51cccf
smc4718 <smc4718@naver.com>
2024-09-04 23:33:27 +0900

    정리 삭제2

--- validation/src/main/java/hello/itemservice/ItemServiceApplication19.java
+++ validation/src/main/java/hello/itemservice/ItemServiceApplication19.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ItemServiceApplication18 {
+public class ItemServiceApplication19 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ItemServiceApplication18.class, args);
+		SpringApplication.run(ItemServiceApplication19.class, args);
 	}
 
 }
