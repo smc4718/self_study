d41a7b4435b048bad3707a429df6e1ee3c750b1e
smc4718 <smc4718@naver.com>
2024-09-04 23:32:31 +0900

    정리 삭제

---
+++ validation/src/main/java/hello/itemservice/ItemServiceApplication18.java
@@ -0,0 +1,13 @@
+package hello.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceApplication18 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceApplication18.class, args);
+	}
+
+}
