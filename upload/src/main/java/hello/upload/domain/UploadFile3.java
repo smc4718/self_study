519ffc50e8c77d89c155a572366facf59ac1cbf1
smc4718 <smc4718@naver.com>
2024-11-21 22:29:05 +0900

    업로드파일3 추가

--- upload/src/main/java/hello/upload/domain/UploadFile3.java
+++ upload/src/main/java/hello/upload/domain/UploadFile3.java
@@ -3,12 +3,12 @@ package hello.upload.domain;
 import lombok.Data;
 
 @Data
-public class UploadFile2 {
+public class UploadFile3 {
 
     private String uploadFileName;
     private String storeFileName;
 
-    public UploadFile2(String uploadFileName, String storeFileName) {
+    public UploadFile3(String uploadFileName, String storeFileName) {
         this.uploadFileName = uploadFileName;
         this.storeFileName = storeFileName;
     }
