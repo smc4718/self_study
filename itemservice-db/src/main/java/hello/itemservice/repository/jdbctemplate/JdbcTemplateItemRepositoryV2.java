d1456f0f05972de141a2726ae46d0df417cc22e1
smc4718 <smc4718@naver.com>
2024-12-21 18:46:00 +0900

    JdbcTemplate - 이름 지정 파라미터1,2

---
+++ itemservice-db/src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV2.java
@@ -0,0 +1,127 @@
+package hello.itemservice.repository.jdbctemplate;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * NamedParameterJdbcTemplate
+ * 파라미터 바인딩을 순서가 아니라 이름 기반으로 연결시켜준다.
+ * 아래 여러가지 방법으로 사용할 수 있다.
+ * SqlParameterSource
+ * - BeanPropertySqlParameterSource
+ * - MapSqlParameterSource
+ * Map
+ *
+ * BeanPropertyRowMapper
+ *
+ */
+@Slf4j
+@Repository
+public class JdbcTemplateItemRepositoryV2 implements ItemRepository {
+
+//    private final JdbcTemplate template;
+    private final NamedParameterJdbcTemplate template;
+
+
+    public JdbcTemplateItemRepositoryV2(DataSource dataSource) {
+        this.template = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Item save(Item item) {
+        String sql = "insert into item(item_name, price, quantity) " +
+                "values(:itemName, :price, :quantity)";
+
+        SqlParameterSource param = new BeanPropertySqlParameterSource(item);
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        template.update(sql, param, keyHolder);
+
+        long key = keyHolder.getKey().longValue();
+        item.setId(key);
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        String sql = "update item " +
+                "set item_name=:itemName, price=:price, quantity=:quantity " +
+                "where id=:id";
+
+        SqlParameterSource param = new MapSqlParameterSource()
+                .addValue("itemName", updateParam.getItemName())
+                .addValue("price", updateParam.getPrice())
+                .addValue("quantity", updateParam.getQuantity())
+                .addValue("id", itemId);
+
+        template.update(sql, param);
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        String sql = "select id, item_name, price, quantity from item where id = :id";
+        try {
+            Map<String, Object> param = Map.of("id", id);
+            Item item = template.queryForObject(sql, param, itemRowMapper());  // queryForObject() 는 하나만 가져올 때 사용.
+            return Optional.of(item);   //Optional.of() 면, ()괄호안이 null이면 안 된다. null이어야 하는 건 Optional.ofNullable() 일 경우에만 null이어야 함.
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();    // = 데이터 결과가 없다. 비어있다. 라는 의미.
+        }
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        SqlParameterSource param = new BeanPropertySqlParameterSource(cond);
+
+        String sql = "select id, item_name, price, quantity from item";
+        //동적 쿼리
+        if (StringUtils.hasText(itemName) || maxPrice != null) {
+            sql += " where";
+        }
+
+        boolean andFlag = false;
+        if (StringUtils.hasText(itemName)) {
+            sql += " item_name like concat('%',:itemName,'%')";
+            andFlag = true;
+        }
+
+        if (maxPrice != null) {
+            if (andFlag) {
+                sql += " and";
+            }
+            sql += " price <= :maxPrice";
+        }
+
+        log.info("sql={}", sql);
+        return template.query(sql,param, itemRowMapper());
+    }
+
+    private RowMapper<Item> itemRowMapper() {
+        return BeanPropertyRowMapper.newInstance(Item.class);   //이렇게 하면 resultSet 가지고 Item 객체에 있는 속성들 이름으로 다 넣어준다. + camel 변환 지원
+    }
+}
