6452e21dfdc1a0f000526053de24126b0b76d2a5
smc4718 <smc4718@naver.com>
2025-01-16 00:53:02 +0900

    Querydsl 적용

--- itemservice-db/src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV3.java
+++ itemservice-db/src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV3.java
@@ -1,56 +1,102 @@
 package hello.itemservice.repository.jpa;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import hello.itemservice.domain.Item;
+import hello.itemservice.domain.QItem;
 import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
+import static hello.itemservice.domain.QItem.item;
+
 @Repository
 @Transactional
-@RequiredArgsConstructor
 public class JpaItemRepositoryV3 implements ItemRepository {
 
-    private final SpringDataJpaItemRepository repository;
+    private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    public JpaItemRepositoryV3(EntityManager em) {
+        this.em = em;
+        this.query = new JPAQueryFactory(em);
+    }
 
     @Override
     public Item save(Item item) {
-        return repository.save(item);
+        em.persist(item);
+        return item;
     }
 
     @Override
     public void update(Long itemId, ItemUpdateDto updateParam) {
-        Item findItem = repository.findById(itemId).orElseThrow();
+        Item findItem = em.find(Item.class, itemId);
         findItem.setItemName(updateParam.getItemName());
         findItem.setPrice(updateParam.getPrice());
         findItem.setQuantity(updateParam.getQuantity());
     }
 
     @Override
-    public Optional<Item> findById(Long id) {   //조회가 될 수도 있고 조회가 안 될수도 있기에 Optional을 쓴 것
-        return repository.findById(id);
+    public Optional<Item> findById(Long id) {
+        Item item = em.find(Item.class, id);
+        return Optional.ofNullable(item);
     }
 
-    @Override
-    public List<Item> findAll(ItemSearchCond cond) {
+    public List<Item> findAllOld(ItemSearchCond cond) {     //올드한 사용법
         String itemName = cond.getItemName();
         Integer maxPrice = cond.getMaxPrice();
 
-        if(StringUtils.hasText(itemName) && maxPrice != null) {
-//            return repository.findByItemNameLikeAndPriceLessThanEqual("%" + itemName + "%", maxPrice);
-            return repository.findItems("%" + itemName + "%", maxPrice);
-        } else if(StringUtils.hasText(itemName)) {
-            return repository.findByItemNameLike("%" + itemName + "%");
-        } else if (maxPrice != null) {
-            return repository.findByPriceLessThanEqual(maxPrice);
-        } else {
-            return repository.findAll();
+        QItem item = QItem.item;
+        BooleanBuilder builder = new BooleanBuilder();
+        if(StringUtils.hasText(itemName)) {
+            builder.and(item.itemName.like("%" + itemName + "%"));
         }
+
+        if (maxPrice != null) {
+            builder.and(item.price.loe(maxPrice));  // loe = less of equal : 작거나 같다.
+        }
+
+        List<Item> result = query.select(item)
+                .from(item)
+                .where(builder)
+                .fetch();
+
+        return result;
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {    //리팩토링해서 더 간결해짐
+
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        return query
+                .select(item)
+                .from(item)
+                .where(likeItemName(itemName), maxPrice(maxPrice))
+                .fetch();
+    }
+
+    private Predicate maxPrice(Integer maxPrice) {
+        if (maxPrice != null) {
+            return item.price.loe(maxPrice);  // loe = less of equal : 작거나 같다.
+        }
+        return null;
+    }
+
+    private BooleanExpression likeItemName(String itemName) {
+        if(StringUtils.hasText(itemName)) {
+            return item.itemName.like("%" + itemName + "%");
+        }
+        return null;
     }
 }
